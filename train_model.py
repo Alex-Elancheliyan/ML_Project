87919fccdba97999ca165353555aa7677bfbfbb3
Alex Elancheliyan <117940315+Alex-Elancheliyan@users.noreply.github.com>
2024-07-12 15:20:20 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import pickle
 
-
+#USING PANDAS LOAD DATASET
 df = pd.read_csv('UsedCarPrice.csv')
 
 
